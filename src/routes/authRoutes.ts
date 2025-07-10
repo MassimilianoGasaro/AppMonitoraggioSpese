f2f03262b20f1e6578582edcee827517f2593eec
Massimiliano Gasaro <massimiliano.gasaro@atomoshyla.com>
2025-07-10 15:10:56 +0200

    add /api in endpoints

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
-import { register, login, logout, test } from '../controllers/authController';
+import { register, login, logout, test, verifyToken, getMe } from '../controllers/authController';
 import { requireAuth } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
+// Rotte pubbliche
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', logout);
-router.get('/', requireAuth, test);
+
+// Rotte protette
+router.post('/logout', verifyToken, logout);
+router.get('/me', verifyToken, getMe);
+router.get('/test', requireAuth, test);
 
 export default router;
